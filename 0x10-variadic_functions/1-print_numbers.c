8347d866c5a5bcce25d78db79ebd3dff1327f9e2
SkamaAFilliy <paulomotayo001@gmail.com>
2022-10-13 16:24:48 -0700

    My second commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,23 +6,23 @@
  * print_numbers - prints all numbers with a separator
  * @separator: separe numbers
  * @n: int, number of undefined arguments
+ *
+ * Return: 0
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index;
+	unsigned int i;
+	int num;
+	va_list valist;
 
-	va_start(nums, n);
-
-	for (index = 0; index < n; index++)
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(nums, int));
-
-		if (index != (n - 1) && separator != NULL)
-			Printf("%s", separator);
+		num = va_arg(valist, int);
+		printf("%d", num);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
 	}
-
-	printf ("\n");
-
-	va_end(nums);
+	printf("\n");
+	va_end (valist);
 }
